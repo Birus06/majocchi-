8c0274c2a65aaab7e014d96392081a4f0ee65e72
Carlo Majocchi <92304499+Birus06@users.noreply.github.com>
2021-11-06 15:10:49 +0100

    Add files via upload

---
+++ majocchi-stringhe-python.py
@@ -0,0 +1,9 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/Windows/AppData/Local/Programs/Python/Python310/majocchi-stringhe-python.py
+casa
+pappa
+scuola
+cding
+python
